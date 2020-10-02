86e4277bf729c29976d0b0a43295614b0ed267d3
Larry N <larryn35@gmail.com>
2020-10-02 14:46:58 -0700

    initial commit

---
+++ DesignCodeCourse/DesignCodeCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeCourse
+//
+//  Created by Larry Nguyen on 10/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeCourse/DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Larry Nguyen on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
