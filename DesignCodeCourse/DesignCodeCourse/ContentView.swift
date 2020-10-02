1ff6c004c6e3bd05920f5edd4ebb9f6570aba51f
Larry N <larryn35@gmail.com>
2020-10-02 15:20:12 -0700

    Lesson 1

--- DesignCodeCourse/DesignCodeCourse/ContentView.swift
+++ DesignCodeCourse/DesignCodeCourse/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text("SwiftUI for iOS 14")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            
+            Text("20 Sections")
+                .foregroundColor(Color.white)
+            
+            
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: 200.0, height: 200.0))
+                .preferredColorScheme(.dark)
+        }
     }
 }
