79d49df357c715c5bc047cb9a624b62986ca02a1
Larry N <larryn35@gmail.com>
2020-10-03 15:14:35 -0700

    Lesson 8 - lazy grid

--- DesignCodeCourse/DesignCodeCourse/CoursesView.swift
+++ DesignCodeCourse/DesignCodeCourse/CoursesView.swift
@@ -16,11 +16,15 @@ struct CoursesView: View {
     var body: some View {
         ZStack {
             ScrollView {
-                VStack(spacing: 20) {
+                LazyVGrid(columns: [
+                    GridItem(.adaptive(minimum: 160), spacing: 16)
+                ],
+                spacing: 16
+                ) {
                     ForEach(courses) { item in
                         CourseItem(course: item)
                             .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(width: 335, height: 250)
+                            .frame(height: 200)
                             .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
                                 withAnimation(.spring()) {
                                     show.toggle()
@@ -31,6 +35,7 @@ struct CoursesView: View {
                             .disabled(isDisabled)
                     }
                 }
+                .padding(16)
                 .frame(maxWidth: .infinity)
             }
             if selectedItem != nil {
