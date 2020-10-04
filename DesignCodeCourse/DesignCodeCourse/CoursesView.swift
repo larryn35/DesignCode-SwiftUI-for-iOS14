3a4502f14744055abf3962c4459a0a1c4adc2f86
Larry N <larryn35@gmail.com>
2020-10-03 17:45:44 -0700

    Lesson 9 - fix issues from lazy grid with matchedGeometryEffect and zIndex

--- DesignCodeCourse/DesignCodeCourse/CoursesView.swift
+++ DesignCodeCourse/DesignCodeCourse/CoursesView.swift
@@ -16,34 +16,54 @@ struct CoursesView: View {
     var body: some View {
         ZStack {
             ScrollView {
-                LazyVGrid(columns: [
-                    GridItem(.adaptive(minimum: 160), spacing: 16)
-                ],
-                spacing: 16
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                    spacing: 16
                 ) {
                     ForEach(courses) { item in
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                            .frame(height: 200)
-                            .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
-                                withAnimation(.spring()) {
-                                    show.toggle()
-                                    selectedItem = item
-                                    isDisabled = true
-                                }
-                            })
-                            .disabled(isDisabled)
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                        show.toggle()
+                                        selectedItem = item
+                                        isDisabled = true
+                                    }
+                                })
+                                .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                     }
                 }
                 .padding(16)
                 .frame(maxWidth: .infinity)
             }
+            .zIndex(1.0)
             if selectedItem != nil {
-                ScrollView {
-                    CourseItem(course: selectedItem!) // can force unwrap due to if selectedItem != nil conditional
-                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
-                        .frame(height: 300)
-                        .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                ZStack(alignment: .topTrailing) {
+                    VStack {
+                        ScrollView {
+                            CourseItem(course: selectedItem!) // can force unwrap due to if selectedItem != nil conditional
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
+                                .frame(height: 300)
+                            VStack {
+                                ForEach(0 ..< 20) { item in
+                                    CourseRow()
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace)
+                    .edgesIgnoringSafeArea(.all)
+                    
+                    CloseButton()
+                        .padding(.trailing, 16)
+                        .onTapGesture(count: 1, perform: {
                             withAnimation(.spring()) {
                                 show.toggle()
                                 selectedItem = nil
@@ -52,30 +72,9 @@ struct CoursesView: View {
                                 }
                             }
                         })
-                    VStack {
-                        ForEach(0 ..< 20) { item in
-                            CourseRow()
-                        }
-                    }
-                    .padding()
                 }
-                .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion:
-                            AnyTransition
-                            .opacity
-                            .animation(Animation.spring()
-                                        .delay(0.3)),
-                        removal:
-                            AnyTransition
-                            .opacity
-                            .animation(.spring())
-                    )
-                )
-                .edgesIgnoringSafeArea(.all)
+                .zIndex(2)
             }
-            
         }
     }
 }
